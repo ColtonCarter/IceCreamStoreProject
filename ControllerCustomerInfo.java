ba6ef0255fa4467e204a48ac83a9cff69c6ca610
ColtonCarter <Colton.Carter1130@gmail.com>
2015-11-02 19:39:23 -0600

    Finished Basic GUI
    Most of the GUI is done, still have to fix some stuff up but
    implementation is next. Removed MainGUI class add Main class

---
+++ ControllerCustomerInfo.java
@@ -0,0 +1,62 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class ControllerCustomerInfo implements Initializable {
+
+	@FXML
+	public Button btn_Select; 
+ 
+	
+	
+    
+
+
+    @Override // This method is called by the FXMLLoader when initialization is complete
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) 
+    {
+    	
+
+        
+    	btn_Select.setOnAction(new EventHandler<ActionEvent>() 
+        {
+        	
+            @Override
+            public void handle(ActionEvent event) 
+            {
+            	
+            	LoadScene("Select", new Stage(), "Menu.fxml", 640, 480);
+            	          
+            }
+        });
+        }
+    
+    public void LoadScene(String newTitle, Stage primaryStage, String fxmlName, int width, int height)
+	{
+		try {
+			
+			Parent newRoot = FXMLLoader.load(getClass().getResource(fxmlName));
+	        Scene scene = new Scene(newRoot, width, height);
+	        
+	        primaryStage.setTitle(newTitle);
+	        primaryStage.setScene(scene);
+
+	        primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+}
---
+++ Main.java
@@ -0,0 +1,64 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+
+
+public class Main extends Application 
+{
+	
+	
+
+	
+	
+	@Override
+	public void start(Stage primaryStage)
+	{
+		
+		
+		
+		
+		LoadScene("Queue", primaryStage, "OrderQueue.fxml", 640, 480);
+	
+		
+		
+		
+        
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+		
+		
+	}
+	
+	
+	void LoadScene(String newTitle, Stage primaryStage, String fxmlName, int width, int height)
+	{
+		try {
+			
+			Parent newRoot = FXMLLoader.load(getClass().getResource(fxmlName));
+	        Scene scene = new Scene(newRoot, width, height);
+	        primaryStage.setTitle(newTitle);
+	        primaryStage.setScene(scene);
+	        primaryStage.show();
+	        
+	        
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	
+	
+	
+
+	}
+}
+
+
+
+
