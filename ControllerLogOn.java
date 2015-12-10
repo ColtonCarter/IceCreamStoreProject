e46dc2525ca17902a33b99af8f2369b9e93e3f74
ColtonCarter <Colton.Carter1130@gmail.com>
2015-12-09 18:52:06 -0600

    Revert "Remove Extraneous files"
    This reverts commit e0c4fff847b80494b081e5b06403876c1efd0015.

---
+++ ControllerLogOn.java
@@ -0,0 +1,42 @@
+package icecreamshop;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class ControllerLogOn implements Initializable {
+
+	@FXML
+	public Button btn_Submit; 
+   
+
+
+    @Override // This method is called by the FXMLLoader when initialization is complete
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) 
+    {
+   
+        
+    	btn_Submit.setOnAction(new EventHandler<ActionEvent>() 
+        {
+        	
+            @Override
+            public void handle(ActionEvent event) 
+            {
+            	
+            	Main.scenes.ShowSelectionStage();
+            }
+        });
+        }
+    
+  
+	
+}
